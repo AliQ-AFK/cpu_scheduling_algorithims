d36451e735613b269e5fc707585c52e6372f290c
ALIQ-AFK <aliiqeblawii@gmail.com>
2024-10-31 15:57:42 +0100

    prints the processes before implenting them into the algothim

--- src/process.c
+++ src/process.c
@@ -14,7 +14,7 @@ void    generate_processes(t_process *processes, unsigned int count)
            return;
         }
         processes[i].pid = i + 1;// assigns different pid for every processes 1 to 20.
-        processes[i].arrival_time = rand() % 50; // high range, allows for more idle state
+        processes[i].arrival_time = rand() % 35; // higher range, allows for more idle state
         processes[i].burst_time = rand() % 10 + 1; // rang [1, 10]. +1 so no zero burst time
         processes[i].priority = rand() % 4; // 0 to 3;
         processes[i].completion_time = 0;
@@ -22,4 +22,18 @@ void    generate_processes(t_process *processes, unsigned int count)
         processes[i].waiting_time = 0;
     }
 
+    printf("Generated Processes:\n");
+    printf("| Process | PID | Arrival Time | Burst Time |\n");
+    printf("|---------|-----|--------------|------------|\n");
+
+    for (unsigned int i = 0; i < count; i++) 
+    {
+        printf("| %7d | %3d | %12d | %10d |\n", 
+            i + 1, 
+            processes[i].pid,
+            processes[i].arrival_time,
+            processes[i].burst_time);
+    }
+    printf("|---------|-----|--------------|------------|\n\n");
 }
+
